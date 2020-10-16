dc95f871ecbdd952c946e8d556dd4258ebcf0e7c
coughdrop1205 <rqisti1205@yahoo.co.jp>
2020-10-16 10:46:09 +0900

    1016 Update from dev

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,62 +1,62 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-    # callback for facebook
-    def facebook
-      callback_for(:facebook)
-    end
+    # # callback for facebook
+    # def facebook
+    #   callback_for(:facebook)
+    # end
   
-    # callback for twitter
-    def twitter
-      callback_for(:twitter)
-    end
+    # # callback for twitter
+    # def twitter
+    #   callback_for(:twitter)
+    # end
   
-    # callback for google
-    def google_oauth2
-      callback_for(:google)
-    end
+    # # callback for google
+    # def google_oauth2
+    #   callback_for(:google)
+    # end
   
-    # common callback method
-    def callback_for(provider)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-      else
-        session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-        redirect_to new_user_registration_url
-      end
-    end
+    # # common callback method
+    # def callback_for(provider)
+    #   @user = User.from_omniauth(request.env["omniauth.auth"])
+    #   if @user.persisted?
+    #     sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+    #     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    #   else
+    #     session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+    #     redirect_to new_user_registration_url
+    #   end
+    # end
   
-    def failure
-      redirect_to root_path
+    # def failure
+    #   redirect_to root_path
+    # end
+
+  def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url
     end
+  end
 
-  # def facebook
-  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
+  private
+  def callback_from(provider)
+    provider = provider.to_s
 
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-  #     set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-  #   else
-  #     session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
 
-  # private
-  # def callback_from(provider)
-  #   provider = provider.to_s
-
-  #   @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-  #   if @user.persisted?
-  #     flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-  #     sign_in_and_redirect @user, event: :authentication
-  #   else
-  #     session["devise.#{provider}_data"] = request.env['omniauth.auth']
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
   
 end
